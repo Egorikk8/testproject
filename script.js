23630c7b6e4d39cab5703d8aa673cbe2bd1f8449
Egorikk8 <92586155+Egorikk8@users.noreply.github.com>
2021-10-15 16:20:23 +0300

    index.html , script.js
    first commit changes index.html
    second commit changes script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+let firstName = null;
+let lastName = null;
+let nickName = "Суперкодер";
+
+// показывает первое определённое значение:
+alert(firstName ?? lastName ?? nickName ?? "Аноним"); // Суперкодер
